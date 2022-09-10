34c32ca48ab612643b5f9bc07045adcdaf0e1a25
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-09-10 22:36:23 +0300

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
 #!/bin/bash
-int main() { char name[] = "Programming is like building a multilingual puzzle" ; puts(name); return 0; }
+int main()
+{
+       	char name[] = "Programming is like building a multilingual puzzle" ;
+       	puts(name);
+       	return 0
+}
+		
