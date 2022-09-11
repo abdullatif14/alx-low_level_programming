210683b5a10c6cf07e277ec7f5527e8f0cc68630
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-09-11 14:02:38 +0300

    Positive & Negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main()
+{ 	/* lets declare our first variable n */
+	int a=100;
+	/*lets use the boolen condition*/
+	if( n > 0) {
+		printf("is positive\n");
+	}else if(n == 0 ){
+		printf("is zero\n")
+	}else if(n < 0){
+	        printf("is negative\n")
+	}	
+	printf("Value of n is :%d\n",n);
+
+	return 0;
+}
