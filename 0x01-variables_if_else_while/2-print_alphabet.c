0a5006bac5b3a2c15df9c476a5482778c2bd192b
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-09-12 00:07:39 -0700

    Lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,34 +1,13 @@
-<<<<<<< HEAD
-#include <studio>
-
-/* print all in lowercase */ 
-
-int main(void)
-{
-	char alpha;
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
-		putchar(alpha);
-	putchar('\n');
-	return(0);
-=======
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+/* print alphabets in lowecase */
 
-int main(void)
+int main(void) 
 {
- 	char x;
+	char(lowcase);
+	for (lowcase = 'a'; lowcase <= 'z' ; lowcase++)
+		putchar(lowcase);
+	putchar('\n');
 
-	for(int x = 'A'; x <= 'Z'; x++)
-{
-    		putchar(x);
-		putchar('\n');}
 	return(0);
 }
->>>>>>> c94af0c883d81066135a8d5f1b4a18b2f5434ec1
