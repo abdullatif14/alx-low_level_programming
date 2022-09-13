a6ba37074ea1ca7d25205d76df88f968c630b497
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-09-13 02:51:36 -0700

    In reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+ /**
+  * main - enter function
+  * Description: print letters in reverse
+  * Return : 0
+  */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'z'; x => 'a'; a--)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
