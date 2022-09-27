a87a49e36a045b6f9a28a235b3614b0ab1c392ac
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-09-27 11:40:17 -0700

    Chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,7 +13,10 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-		_putchar(a[i][j]);
+		for (i = 0; i < 8; i++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
