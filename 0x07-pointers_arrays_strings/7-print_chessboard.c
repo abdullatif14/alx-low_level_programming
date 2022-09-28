152625ded6316270fd9e94a01b1bc5fec96807bc
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-09-27 23:11:37 -0700

    Chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,16 +10,14 @@
 
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i, m = 0;
+	unsigned int i, j;
 
-	for (i = 0; i < 64; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (i % 8 == 0 && i != 0)
+		for (j = 0; j < 8; j++)
 		{
-			m = i;
-			_putchar('\n');
+			_putchar(a[i][j]);
 		}
-		_putchar(a[i / 8][i - m]);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
