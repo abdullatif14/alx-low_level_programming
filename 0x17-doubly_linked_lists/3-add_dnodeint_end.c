2c9fe08726ae2da7c2ebba85c3130d71f8d086c3
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-12-12 03:07:24 -0800

    New node at the end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,29 +10,31 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *h;
+	dlistint_t *new;
 
-	if (new_node == NULL)
-
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->next = NULL;
-	if (*head == NULL)
+	new->n = n;
+	new->next = NULL;
+
+	h = *head;
+
+	if (h != NULL)
 	{
-		new_node->prev = NULL;
-		*head = new_node;
-		return (new_node);
+		while (h->next != NULL)
+			h = h->next;
+		h->next = new;
+	}
+	else
+	{
+		*head = new;
 	}
 
+	new->prev = h;
 
-	dlistint_t *last = *head;
-
-	while (last->next != NULL)
-		last = last->next;
-
-	last->next = new_node;
-	new_node->prev = last;
-
-	return (new_node);
+	return (new);
 }
+~
